00286a981348a6a767527949e3b8be4ade03c0ff
Masaya Kataoka <ms.kataoka@gmail.com>
2024-07-23 00:01:03 +0900

    comment out ethernet connection function for test
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- motor_driver_firmware/ouxt_motor_driver/Core/Src/main.c
+++ motor_driver_firmware/ouxt_motor_driver/Core/Src/main.c
@@ -347,36 +347,42 @@ static void MX_GPIO_Init(void)
 void StartDefaultTask(void const * argument)
 {
   /* init code for LWIP */
-  MX_LWIP_Init();
+  // MX_LWIP_Init();
   /* USER CODE BEGIN 5 */
   /// This code from https://zenn.dev/legityew/articles/080680f2539068#%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%A0
   /// It will be replace in the near future
-  uint8_t rxbuf[4];
-  uint8_t txbuf[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-  struct sockaddr_in rxAddr,txAddr;
-  int socket = lwip_socket(AF_INET, SOCK_DGRAM, 0);
-  memset((char*) &txAddr, 0, sizeof(txAddr));
-  memset((char*) &rxAddr, 0, sizeof(rxAddr));
-
-  //アドレスの構造体のデータを定義
-  rxAddr.sin_family = AF_INET; //プロトコルファミリの設定(IPv4に設定)
-  rxAddr.sin_len = sizeof(rxAddr); //アドレスのデータサイズ
-  rxAddr.sin_addr.s_addr = INADDR_ANY; //アドレスの設定(今回はすべてのアドレスを受け入れるためINADDR_ANY)
-  rxAddr.sin_port = lwip_htons(PC_PORT); //ポートの指定
-  txAddr.sin_family = AF_INET; //プロトコルファミリの指定(IPv4に設定)
-  txAddr.sin_len = sizeof(txAddr); //アドレスのデータのサイズ
-  txAddr.sin_addr.s_addr = inet_addr(PC_ADDR); //アドレスの設定
-  txAddr.sin_port = lwip_htons(PC_PORT); //ポートの指定
-  (void)lwip_bind(socket, (struct sockaddr*)&rxAddr, sizeof(rxAddr)); //IPアドレスとソケットを紐付けて受信をできる状態に
-  socklen_t n; //受信したデータのサイズ
-  socklen_t len = sizeof(rxAddr); //rxAddrのサイズ
+//  uint8_t rxbuf[4];
+//  uint8_t txbuf[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+//  struct sockaddr_in rxAddr,txAddr;
+//  int socket = lwip_socket(AF_INET, SOCK_DGRAM, 0);
+//  memset((char*) &txAddr, 0, sizeof(txAddr));
+//  memset((char*) &rxAddr, 0, sizeof(rxAddr));
+//
+//  //アドレスの構造体のデータを定義
+//  rxAddr.sin_family = AF_INET; //プロトコルファミリの設定(IPv4に設定)
+//  rxAddr.sin_len = sizeof(rxAddr); //アドレスのデータサイズ
+//  rxAddr.sin_addr.s_addr = INADDR_ANY; //アドレスの設定(今回はすべてのアドレスを受け入れるためINADDR_ANY)
+//  rxAddr.sin_port = lwip_htons(PC_PORT); //ポートの指定
+//  txAddr.sin_family = AF_INET; //プロトコルファミリの指定(IPv4に設定)
+//  txAddr.sin_len = sizeof(txAddr); //アドレスのデータのサイズ
+//  txAddr.sin_addr.s_addr = inet_addr(PC_ADDR); //アドレスの設定
+//  txAddr.sin_port = lwip_htons(PC_PORT); //ポートの指定
+//  (void)lwip_bind(socket, (struct sockaddr*)&rxAddr, sizeof(rxAddr)); //IPアドレスとソケットを紐付けて受信をできる状態に
+//  socklen_t n; //受信したデータのサイズ
+//  socklen_t len = sizeof(rxAddr); //rxAddrのサイズ
 
   /* Infinite loop */
+  double ratio = -1.0;
   for(;;)
   {
-	n = lwip_recvfrom(socket, (uint8_t*) rxbuf, sizeof(rxbuf), (int) NULL, (struct sockaddr*) &rxAddr, &len); //受信処理(blocking)
-	lwip_sendto(socket, (uint8_t*) txbuf, sizeof(txbuf), 0, (struct sockaddr*) &txAddr, sizeof(txAddr)); //受信したら送信する
-	motorSetSpeed(&motor, 0.3);
+	if(ratio >= 1.0) {
+		ratio = -1.0;
+	}else {
+		ratio = ratio + 0.01;
+	}
+//	n = lwip_recvfrom(socket, (uint8_t*) rxbuf, sizeof(rxbuf), (int) NULL, (struct sockaddr*) &rxAddr, &len); //受信処理(blocking)
+//	lwip_sendto(socket, (uint8_t*) txbuf, sizeof(txbuf), 0, (struct sockaddr*) &txAddr, sizeof(txAddr)); //受信したら送信する
+	motorSetSpeed(&motor, ratio);
     osDelay(10);
   }
   /* USER CODE END 5 */
